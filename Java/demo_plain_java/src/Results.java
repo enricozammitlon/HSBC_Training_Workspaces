2bdd61418b5acd75c35ddd9fbeea5c194851f40a
Enrico Zammit Lonardelli <enrico.zammitl@gmail.com>
2020-10-26 12:59:39 +0000

    Add java environment template

---
+++ Java/demo_plain_java/src/Results.java
@@ -0,0 +1,13 @@
+class Results {
+    public static void main(String[] args){
+        int physics,chemistry,maths,total;
+        float percentage;
+        physics=90;
+        chemistry=52;
+        maths=91;
+        total=physics+chemistry+maths;
+        percentage = (float) total/3;
+        System.out.println(total);
+        System.out.println(percentage);
+    }
+}
